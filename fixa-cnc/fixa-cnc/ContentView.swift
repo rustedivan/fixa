a5004ff4261f8921773440257ccd529e6a10ba6f
Ivan Milles <ivan@rusted.se>
2020-07-25 11:05:20 +0200

    Very rough setup of controls

--- fixa-cnc/fixa-cnc/ContentView.swift
+++ fixa-cnc/fixa-cnc/ContentView.swift
@@ -9,15 +9,49 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, World!")
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-    }
+	var body: some View {
+		VStack {
+			HStack {
+				VStack(alignment: .leading) {
+					Text("TapMap").font(.title)
+					Text("on Raido").font(.subheadline)
+				}
+				Spacer()
+			}
+			BorderControls(label: "Continent border")
+			BorderControls(label: "Country border")
+			BorderControls(label: "Province border")
+			Spacer()
+		}.padding(16.0)
+		 .frame(minWidth: 320.0)
+	}
 }
 
+struct BorderControls: View {
+	let label: String
+	let sliderWidth: CGFloat = 200.0
+	var body: some View {
+		GroupBox(label: Text(label)) {
+			HStack {
+				Text("Width")
+				Spacer()
+				Slider(value: .constant(2.0), in: 0.1 ... 10.0)
+					.frame(width: sliderWidth)
+			}
+			HStack {
+				Text("Brightness")
+				Spacer()
+				Slider(value: .constant(0.5), in: 0.0 ... 1.0)
+					.frame(width: sliderWidth)
+			}
+		}
+		.padding(.top, 16.0)
+	}
+}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+			ContentView()
+				.frame(width: 400.0, height: 600.0)
     }
 }
